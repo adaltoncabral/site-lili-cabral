92f55327099614450c0928166f93ace4d3fcdc7a
adaltoncabral <adaltoncabral@gmail.com>
2025-06-20 09:59:37 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    formats: ['image/webp'],
+    domains: ['lilicabral.com.br'], // adicione domínios externos se usar imagens externas
+  },
+  trailingSlash: true,
+  experimental: {
+    scrollRestoration: true
+  }
+}
 
 module.exports = nextConfig
