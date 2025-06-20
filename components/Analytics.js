3d65c0f3c159233a2eec4bf273a711cc44e451fd
adaltoncabral <adaltoncabral@gmail.com>
2025-06-20 09:46:17 -0300

    Update Analytics.js

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,25 +1,25 @@
-
 import Script from 'next/script';
 
 export default function Analytics() {
   return (
     <>
+      {/* Google Analytics Script */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-R9MZ3Y853D`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-R9MZ3Y853D"
       />
       <Script
         id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-R9MZ3Y853D', {
-            page_path: window.location.pathname,
-          });
-        `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R9MZ3Y853D', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
       />
     </>
