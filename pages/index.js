d5c1c92d4cd662f702e2b97ea06463c6bf4fb3e3
adaltoncabral <adaltoncabral@gmail.com>
2025-06-22 18:51:59 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'; import Header from '../components/Header'; import Footer from '../components/Footer'; import FloatingButton from '../components/FloatingButton'; import Analytics from '../components/Analytics'; import Image from 'next/image'; import Link from 'next/link'; import { useEffect, useState } from 'react';
 
-const banners = [ '/banner1.jpg', '/banner2.jpg', '/banner3.jpg' ];
+const banners = [ '/banners/banner1.jpg', '/banners/banner2.jpg', '/banners/banner3.jpg' ];
 
 export default function Home() { const [current, setCurrent] = useState(0);
 
@@ -31,25 +31,25 @@ return ( <> <Head> <title>Lili Cabral – Conforto que Presenteia</title> <meta 
     <section style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(160px, 1fr))', gap: '1.5rem', padding: '2rem 1rem', maxWidth: '1000px', margin: '0 auto', textAlign: 'center' }}>
       <Link href="/catalogo?pijamas">
         <div>
-          <Image src="/icon-pijama.png" alt="Pijamas" width={64} height={64} />
+          <Image src="/icones/icon-pijama.png" alt="Pijamas" width={64} height={64} />
           <p>Pijamas</p>
         </div>
       </Link>
       <Link href="/catalogo?lingeries">
         <div>
-          <Image src="/icon-lingerie.png" alt="Lingeries" width={64} height={64} />
+          <Image src="/icones/icon-lingerie.png" alt="Lingeries" width={64} height={64} />
           <p>Lingeries</p>
         </div>
       </Link>
       <Link href="/catalogo?promocoes">
         <div>
-          <Image src="/icon-promocao.png" alt="Promoções" width={64} height={64} />
+          <Image src="/icones/icon-promocao.png" alt="Promoções" width={64} height={64} />
           <p>Promoções</p>
         </div>
       </Link>
       <Link href="/catalogo?colecoes">
         <div>
-          <Image src="/icon-especial.png" alt="Coleções Especiais" width={64} height={64} />
+          <Image src="/icones/icon-especial.png" alt="Coleções Especiais" width={64} height={64} />
           <p>Coleções Especiais</p>
         </div>
       </Link>
@@ -62,4 +62,4 @@ return ( <> <Head> <title>Lili Cabral – Conforto que Presenteia</title> <meta 
 
 ); }
 
-              
+      
